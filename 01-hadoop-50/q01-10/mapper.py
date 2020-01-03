c06f98b2bb35aaa8b7d2506fa55ce565e520f149
juanrpo19 <juanrpo19@hotmail.com>
2020-01-03 01:24:02 -0500

    Revert "Punto 1 - V1.0"
    This reverts commit 546e1d87d4e6b1c31e3ad43b9f02532dbc0539e3.

--- 01-hadoop-50/q01-10/mapper.py
+++ 01-hadoop-50/q01-10/mapper.py
@@ -1,6 +1,4 @@
 import sys
-
-if __name__ == "__main__":
-    for line in sys.stdin:
-        valor_columna = line.split(',')[2]
-        sys.stdout.write("{}\t1\n".format(valor_columna))
+#
+# >>> Escriba el codigo del mapper a partir de este punto <<<
+#
