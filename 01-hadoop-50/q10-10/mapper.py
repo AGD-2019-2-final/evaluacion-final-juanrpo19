ef97484f504f8cf0103d8851d05bbf63cc00c213
juanrpo19 <juanrpo19@hotmail.com>
2020-01-03 17:57:58 -0500

    02 20190103

--- 01-hadoop-50/q10-10/mapper.py
+++ 01-hadoop-50/q10-10/mapper.py
@@ -1,5 +1,16 @@
 import sys
 #
-# >>> Escriba el codigo del mapper a partir de este punto <<<
+#  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-        
+if __name__ == "__main__":
+    for line in sys.stdin:
+        numero = line.split('\t')[0]
+        letras = line.split('\t')[1]
+        #letras = letras.split('\n')[0]
+        #letras = letras.split('\n')[0]
+
+        #letras=str(letras)
+        #lon=len(letras)
+        numero=numero.zfill(3)
+        sys.stdout.write("{}\t{}".format(numero,letras))
+    sys.stdout.write('\n')
--- 01-hadoop-50/q10-10/reducer.py
+++ 01-hadoop-50/q10-10/reducer.py
@@ -1,4 +1,39 @@
 import sys
 #
-# >>> Escriba el codigo del mapper a partir de este punto <<<
+# >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+if __name__ == '__main__':
+
+    curkey = None
+    total = 0
+    #letras='A,C'
+    array=''
+    nletra=''
+
+    ##
+    ## cada linea de texto recibida es una
+    ## entrada clave \tabulador valor
+    ##
+    for line in sys.stdin:
+
+        numero = line.split("\t")[0]
+        val = line.split("\t")[1]
+        letras= val
+        #letras.sort()
+        #maximo=max(letras)
+        
+        
+        #sys.stdout.write("{}\t{}\n\n\n".format("A en posicion: ",pos))
+        #sys.stdout.write("{}".format(letras))
+        for i in range(len(letras)):
+            #buscar=letras.find(letras[i][:1])
+            #sys.stdout.write("\n{}".format(buscar))
+            #sys.stdout.write("{}\t{}\t{}\n".format("Letra: ",letras[i][:1],buscar))
+            if (array.find(letras[i][:1]) == -1) and letras[i][:1]!=',':
+                nletra=letras[i][:1]
+                #sys.stdout.write("{}\t{}\t{}\n".format("Letra: ",letras[i][:1],buscar))
+                
+                array=nletra + array 
+    sys.stdout.write("{}".format(array))
+
