9366800dc8c16e8500774fb44bafeab0a67bfc3d
juanrpo19 <juanrpo19@hotmail.com>
2020-01-24 16:11:36 -0500

    08 Avance Final 20200124

--- 01-hadoop-50/q07-10/mapper.py
+++ 01-hadoop-50/q07-10/mapper.py
@@ -6,8 +6,8 @@ if __name__=="__main__":
 	for linea in sys.stdin:
 		letra=linea.split("   ")[0]
 		fecha=linea.split("   ")[1]
-		valor=linea.split("   ")[2]
-		valor=int(valor)
-		
-
-		sys.stdout.write("{}\t{}\t{}\n".format(letra,valor,fecha))
+		numero=int(linea.split("   ")[2])
+		numero=str(numero)
+		numero=numero.zfill(3)
+		concat=letra+numero
+		sys.stdout.write("{}\t{}\t{}\t{}\n".format(concat,letra,fecha,numero))
--- 01-hadoop-50/q07-10/reducer.py
+++ 01-hadoop-50/q07-10/reducer.py
@@ -3,14 +3,6 @@ import sys
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 if __name__ == '__main__':
-
-    curkey = None
-    valor_mayor = 0
-    fecha_mayor=''
-    llave_mayor=''
-    valor_menor = 0
-    fecha_menor=''
-    llave_menor=''
    
 
     ##
@@ -19,28 +11,8 @@ if __name__ == '__main__':
     ##
     for line in sys.stdin:
 
-        key,val,date= line.split("\t")
+        concat,key,date,val= line.split("\t")
         val=int(val)
-        
-        if curkey is None:
-            llave_mayor=key
-            fecha_mayor=date
-            val=int(val)
-            valor_mayor=val
 
-        if key == llave_mayor:
-            if valor_mayor <= val:
-                llave_menor=llave_mayor
-                fecha_menor=fecha_mayor
-                valor_menor=valor_mayor
-
-                llave_mayor=key
-                fecha_mayor=date
-                valor_mayor=val
-
-                curkey=key
-
-                sys.stdout.write("{}\t{}\t{}".format(llave_menor, fecha_menor ,valor_menor))
-
-    sys.stdout.write("{}\t{}\t{}\n".format(llave_menor, fecha_menor ,valor_menor))
+        sys.stdout.write("{}   {}   {}\n".format(key,date ,val))
             
